6d0059863efe8d355830663d6850733c32ef5bae
Dustella <fdnoaivj@outlook.com>
2022-01-19 15:30:44 +0800

    sync temp changes

---
+++ src/Landing.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-row justify="end">
+        <el-col :span="8">
+          <div class="topbar-title">
+            <span
+              style="
+                color: green;
+                font-size: 28px;
+                font-family: Microsoft YaHei;
+              "
+              >麦趣测评</span
+            >
+          </div>
+        </el-col>
+        <el-col :span="8" />
+        <el-col :span="8">
+          <el-link>
+            <router-link to="/">Home</router-link>
+          </el-link>
+          |
+          <router-link to="/about">About</router-link>
+        </el-col>
+      </el-row>
+    </el-header>
+
+    <el-main>
+      <router-view />
+    </el-main>
+  </el-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style lang="scss"></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import personalCenter from "../views/PersonalCenter.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,13 +12,19 @@ const routes: Array<RouteRecordRaw> = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/personalCenter",
+    name: "Personal Center",
+    component: personalCenter,
   },
 ];
 
