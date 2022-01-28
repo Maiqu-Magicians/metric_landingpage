31435efd6f492de4d851c6160c247e293090f0e2
Dustella <fdnoaivj@outlook.com>
2022-01-28 20:16:06 +0800

    我的产品 基本完成

--- src/views/PersonalCenter/Products.vue
+++ src/views/PersonalCenter/Products.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="right-card"></div>
+  <div class="right-card">
+    <div class="right-card-content">
+      <div v-for="i in product_list" class="product-card" :key="i">
+        <div
+          :style="{
+            position: 'relative',
+            margin: '0 0 0 0',
+            height: '100%',
+            width: '100%',
+            overflow: 'hidden',
+            'border-radius': '3vh',
+            'background-image': 'url(' + i.backgd_url + ')',
+            'background-repeat': 'no-repeat',
+            'background-size': 'cover',
+          }"
+        >
+          <div class="product-card-bottom">
+            <div class="product-card-name">{{ i.name }}</div>
+            <img
+              src="@/assets/btn_login.svg"
+              class="product-card-btn"
+              style="cursor: pointer"
+              @click="openn(i.target_url)"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,8 +35,42 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Products",
+  mounted() {
+    this.product_list[0].backgd_url =
+      "https://pic3.zhimg.com/v2-0e251bb643e6850008477dc97dac550e_r.jpg";
+    this.product_list[0].target_url = "https://github.com/";
+    this.product_list[1].backgd_url =
+      "https://pic3.zhimg.com/v2-0e251bb643e6850008477dc97dac550e_r.jpg";
+    this.product_list[1].target_url = "https://github.com/";
+    this.product_list[2].backgd_url =
+      "https://pic3.zhimg.com/v2-0e251bb643e6850008477dc97dac550e_r.jpg";
+    this.product_list[2].target_url = "https://github.com/";
+  },
   data() {
-    return {};
+    return {
+      product_list: [
+        {
+          name: "Product1",
+          backgd_url: "http://",
+          target_url: "http://",
+        },
+        {
+          name: "Product1",
+          backgd_url: "http://",
+          target_url: "http://",
+        },
+        {
+          name: "Product1",
+          backgd_url: "http://",
+          target_url: "http://",
+        },
+      ],
+    };
+  },
+  methods: {
+    openn(url: string) {
+      window.open(url);
+    },
   },
 });
 </script>
@@ -24,5 +86,51 @@ export default defineComponent({
   opacity: 1;
   border-radius: 30px;
   overflow: hidden;
+}
+
+.right-card-content {
+  position: relative;
+  width: 90%;
+  height: 100%;
+  margin: 3vh auto;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  /* border: 1px solid; */
+}
+
+.product-card {
+  position: relative;
+  width: 30%;
+  height: 39%;
+  margin: 3vh auto;
+  border: 1px solid;
+  border-radius: 3vh;
+}
+
+.product-card-name {
+  position: relative;
+  margin: 10% auto;
+  text-align: center;
+  color: white;
+  font-size: 150%;
+  opacity: 1;
+}
+
+.product-card-bottom {
+  position: relative;
+  height: 50%;
+  overflow: hidden;
+  background: rgba(0, 0, 0, 0.39);
+  margin: 80% 0 0 0;
+}
+
+.product-card-btn {
+  position: relative;
+  left: 50%;
+  top: 10%;
+  transform: translate(-50%, -50%);
+  margin: 0 auto;
+  /* border: 1px solid; */
 }
 </style>
