0243ee6dc5c895d30a5ab16d3724a4f5893c84a7
Dustella <fdnoaivj@outlook.com>
2022-03-04 17:57:42 +0800

    添加主体备案号

--- src/components/base/footer.vue
+++ src/components/base/footer.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="main-footer">
-    <el-row justify="center" style="top: 30%">
+    <el-row justify="center" style="top: 15%">
       <el-link>联系我们</el-link>
       <span style="white-space: pre-wrap"> | </span>
       <el-link>服务条款</el-link>
       <span style="white-space: pre-wrap"> | </span>
       <el-link>隐私保护指引</el-link>
     </el-row>
-    <el-row justify="center" style="top: 40%">
+    <el-row justify="center" style="top: 25%">
       <span> Copyright © 2021-2022 Metric All Rights Reserved.</span>
+    </el-row>
+    <el-row justify="center" style="top: 30%">
+      <el-link
+        to="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11000002000001"
+        >苏ICP备2021050657号-1</el-link
+      >
     </el-row>
   </div>
 </template>
@@ -16,7 +22,8 @@
 :deep(.el-link--inner) {
   font-size: 1.7vh;
   color: #ffffff;
-  font-family: OPPOSans;
+  /* font-family: OPPOSans; */
+  text-decoration: none;
 }
 
 span {
